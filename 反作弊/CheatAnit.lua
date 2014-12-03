971c91457a740763f54e3fc61f0191ba56d716c3
Eric_Lian <505311335@qq.com>
2014-12-03 17:09:18 +0800

    反作弊3.1
    额

---
+++ 反作弊/CheatAnit.lua
@@ -0,0 +1,19 @@
+﻿--[[
+标题: ParaCraft反作弊引擎
+版本：Alpha 0.0.1 [001]
+作者: Eric
+创建时间: 2014年10月03日
+最后修改时间: 2014年10月03日
+描述: 反作弊引擎主体
+Eric_Lian <https://github.com/ExerciseBook> 版权所有©
+]]--
+function main(entity)
+	local mode= GameLogic.GameMode:GetMode()	
+	if mode== 'editor' then 
+			local script = blocks.getscript(19741, 90 , 20193, true)
+			script.cheats=(script.cheats or 0) +1;
+			cmd("/tip 请遵守游戏规则，不要作弊",nil,entity)
+			cmd(string.format("/tip 累计检测作弊次数%d",script.cheats),nil,entity)
+			cmd("/mode",nil,entity)
+	end
+end
---
+++ 反作弊/CheatClean.lua
@@ -0,0 +1,14 @@
+﻿--[[
+标题: ParaCraft反作弊引擎
+版本：Alpha 0.0.1 [001]
+作者: Eric
+创建时间: 2014年10月03日
+最后修改时间: 2014年10月03日
+描述: 反作弊引擎清空纪录
+Eric_Lian <https://github.com/ExerciseBook> 版权所有©
+]]--
+function main(entity)
+	local script = blocks.getscript(19741, 90 , 20193, true)
+	script.cheats=0;
+	cmd("/tip 作弊次数统计已清空",nil,entity)
+end
