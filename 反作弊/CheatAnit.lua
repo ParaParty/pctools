81e775771b60a28cfd72d7217ee84fe46ee5d8af
Eric_Lian <505311335@qq.com>
2015-01-17 15:26:09 +0800

    2015-01-03 补推送
    2015-01-03
    ADD :  反作弊  : [003] : 元旦快乐！新增开关
    ADD :  反作弊  : [003] : 使用gameloop高速循环

--- 反作弊/CheatAnit.lua
+++ 反作弊/CheatAnit.lua
@@ -2,7 +2,7 @@
 Copyright (C) 2014 LianHongrui
 
 标题: ParaCraft反作弊引擎
-版本：Alpha 0.0.1 [001]
+版本：Alpha 0.0.3 [003]
 作者: Eric，哟嘿
 创建时间: 2014年10月03日
 最后修改时间: 2014年10月03日
@@ -10,12 +10,13 @@ Copyright (C) 2014 LianHongrui
 Eric_Lian <https://github.com/ExerciseBook> 版权所有©
 ]]--
 function main(entity)
-	local mode= GameLogic.GameMode:GetMode()	
-	if mode== 'editor' then 
-			local script = blocks.getscript(19741, 90 , 20193, true)
+	local mode= GameLogic.GameMode:GetMode();
+	local script = blocks.getscript(19741, 90 , 20193, true);
+	if (mode== 'editor') and (script.on==1) then 
 			script.cheats=(script.cheats or 0) +1;
-			cmd("/tip 请遵守游戏规则，不要作弊",nil,entity)
-			cmd(string.format("/tip 累计检测作弊次数%d",script.cheats),nil,entity)
-			cmd("/mode",nil,entity)
+			cmd("/tip 请遵守游戏规则，不要作弊",nil,entity);
+			cmd(string.format("/tip 累计检测作弊次数%d",script.cheats),nil,entity);
+			cmd("/mode",nil,entity);
 	end
+	cmd("/activate ~ ~ ~",nil,entity);
 end
--- 反作弊/CheatOFF.lua
+++ 反作弊/CheatOFF.lua
@@ -6,11 +6,10 @@ Copyright (C) 2014 LianHongrui
 作者: Eric
 创建时间: 2014年10月03日
 最后修改时间: 2014年10月03日
-描述: 反作弊引擎清空纪录
+描述: 反作弊关
 Eric_Lian <https://github.com/ExerciseBook> 版权所有©
 ]]--
 function main(entity)
-	local script = blocks.getscript(19741, 90 , 20193, true)
-	script.cheats=0;
-	cmd("/tip 作弊次数统计已清空",nil,entity)
+	local script = blocks.getscript(19741, 90 , 20193, true);
+	script.on=0;
 end
